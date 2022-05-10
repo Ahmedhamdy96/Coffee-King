3aeab1ff230451155547d08e6d58807aeda2be36
Ahmedhamdy96 <es.ahmedhamdy.19@gmail.com>
2022-05-10 07:33:57 +0200

    home logo

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,11 +1,15 @@
 import React from "react";
+import homeLogo from "./../../imgs/home-2-logo-1.png";
 import seprator from "./../../imgs/coffee_cut_long.png";
+
 import style from "./Home.module.css";
 
 const Home = () => {
   return (
     <section className={style.sectionHome}>
-      <div className="container"> Home </div>
+      <div className="container">
+        <img src={homeLogo} className={style.homeLogo} alt="home logo" />
+      </div>
 
       <img src={seprator} className={style.seprator} alt="seprator image" />
     </section>
