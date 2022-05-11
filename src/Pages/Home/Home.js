6d6f3edcab6bb8a5be79d5b2df77c20997f82ac6
Ahmedhamdy96 <es.ahmedhamdy.19@gmail.com>
2022-05-11 05:53:48 +0200

    nav responsive

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -3,11 +3,13 @@ import homeLogo from "./../../imgs/home-2-logo-1.png";
 import seprator from "./../../imgs/coffee_cut_long.png";
 
 import style from "./Home.module.css";
+import Navigation from "../../Components/Navigation/Navigation";
 
 const Home = () => {
   return (
     <section className={style.sectionHome}>
       <div className="container">
+        <Navigation />
         <img src={homeLogo} className={style.homeLogo} alt="home logo" />
       </div>
 
