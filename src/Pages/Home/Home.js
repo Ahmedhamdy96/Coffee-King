045345a51dc9e5ce7a6656455acc09199fe5108c
Ahmedhamdy96 <es.ahmedhamdy.19@gmail.com>
2022-05-12 04:43:34 +0200

    About and features

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -1,20 +1,26 @@
-import React from "react";
+import React, { Fragment } from "react";
 import homeLogo from "./../../imgs/home-2-logo-1.png";
 import seprator from "./../../imgs/coffee_cut_long.png";
 
 import style from "./Home.module.css";
 import Navigation from "../../Components/Navigation/Navigation";
+import Btn from "../../Components/UI/Btn/Btn";
+import About from "../../Components/About/About";
 
 const Home = () => {
   return (
-    <section className={style.sectionHome}>
-      <div className="container">
-        <Navigation />
-        <img src={homeLogo} className={style.homeLogo} alt="home logo" />
-      </div>
+    <Fragment>
+      <section className={style.sectionHome}>
+        <div className="container">
+          <Navigation />
+          <img src={homeLogo} className={style.homeLogo} alt="home logo" />
+          <Btn className={style.cta}> View products </Btn>
+        </div>
 
-      <img src={seprator} className={style.seprator} alt="seprator image" />
-    </section>
+        <img src={seprator} className={style.seprator} alt="seprator image" />
+      </section>
+      <About />
+    </Fragment>
   );
 };
 
